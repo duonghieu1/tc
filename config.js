e432c64eb7540eea5b0d3127129bae003fe29c1b
duonghieu1 <92351964+duonghieu1@users.noreply.github.com>
2022-04-01 20:39:15 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Thư ơi!!',
+    introDesc: `Cậu phải chọn thật lòng đó nhaaaaa ❤`,
+    btnIntro: 'CLICK <3333333',
+    title: 'Phải chăng cậu đã yêu tớ ngay từ cái nhìn đầu tiên ❤',
+    desc: 'Phải chăng cậu đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu mất rồi <33',
+    btnNo: 'Không, bạn bè à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tớ vậy ❤',
+    btnReply: 'Gửi cho tớ nha :3',
+    reply: 'Tại vì cậu đẹp trai ó <33333333',
+    mess: 'Cậu làm bạn gái tớ nhaa ',
+   
+    btnAccept: 'Đồng ý nà ❤',
+    messLink: 'https://www.messenger.com/t/100026579676090/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
